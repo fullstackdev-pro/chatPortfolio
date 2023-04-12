a080ba1266a6cbab4cfe9109aa7a251cc4847bbf
fullstackdev-pro <imomovismoi911@gmail.com>
2023-04-12 06:50:09 +0500

    Birinchi yuklanish

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx, html}"],
+	corePlugins: {
+		preflight: false,
+	},
+	important: "#root",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
